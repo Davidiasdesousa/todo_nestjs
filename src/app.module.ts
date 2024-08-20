b8e117f7cbcb5e69ed49563fb1a74659b5cb3d72
Davi Sousa <davidiasdesousa@gmail.com>
2024-08-20 09:40:57 -0300

    Projeto Completo

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
+import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
+
+@Module({
+  imports: [TaskModule, UsersModule, AuthModule, ConfigModule.forRoot({ isGlobal: true }), DbModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
+})
+export class AppModule { }
---
+++ src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/db/entities/users.entity';
+
+@Module({
+  providers: [UsersService],
+  imports:[TypeOrmModule.forFeature([UserEntity])],
+  exports: [UsersService],
+  controllers: [UsersController]
+})
+export class UsersModule {}
